7270106952eb5fb9d2d7caf598b480e103e440d9
vagrant <vagrant@precise32.(none)>
2014-02-22 20:10:51 +0000

    ident changes from hai

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ app.get("/interviewerSurvey", routes.dosurveyInterviewer);
 
 app.get("/startInterview/:match",routes.kickoff);
 app.get("/startInterviewWithInterviewee/:match",routes.kickoffWithInterviewee);
+app.get("/feedback/:match",routes.postFeedback);
 
 app.get("/login", routes.viewLogin);
 app.get("/unimplemented", routes.viewUnimplemented);
